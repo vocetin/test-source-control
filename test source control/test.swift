34d593433252d635c7af4397f4214c8d87aa184a
Leho Vincent <lehovincent@icloud.com>
2018-06-19 19:33:36 +0200

    class test

---
+++ test source control/test.swift
@@ -0,0 +1,16 @@
+//
+//  test.swift
+//  test source control
+//
+//  Created by Vincent Leho on 19/06/2018.
+//  Copyright © 2018 Vincent Leho. All rights reserved.
+//
+
+import Foundation
+
+
+class Test {
+    func hello() {
+        print("salut")
+    }
+}
